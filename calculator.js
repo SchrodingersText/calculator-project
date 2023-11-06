1aaa25ea1c97e78034911e88018cda2a847f6942
SchrodingersText <130055324+SchrodingersText@users.noreply.github.com>
2023-11-06 16:26:42 +0100

    Create add, subtract, multiply & divide functions

--- calculator.js
+++ calculator.js
@@ -1,2 +1,17 @@
+const add = function (num1, num2) {
+    return num1 + num2;
+};
 
+const subtract = function (num1, num2) {
+    return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+    return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+    if (num1 === 0 && num2 === 0) return "Error";
+    return num1 / num2;
+};
 
