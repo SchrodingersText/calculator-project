403c31efa34abecc380b0825d4420862c9977bfb
SchrodingersText <130055324+SchrodingersText@users.noreply.github.com>
2023-11-08 11:06:21 +0100

    Create a working "operate" function

--- calculator.js
+++ calculator.js
@@ -15,8 +15,28 @@ const divide = function (num1, num2) {
     return num1 / num2;
 };
 
-let firstOperand;
+let firstOperand;   // use an event listener to assign a user-entered value (clicked buttons) to these variables?
 
 let operator;
 
 let secondOperand;
+
+const operate = function (firstOperand, operator, secondOperand) {
+   switch (operator) {
+    case "+":
+        return add(firstOperand, secondOperand);
+        break;
+
+    case "-":
+        return subtract(firstOperand, secondOperand);
+        break;
+
+    case "*":
+        return multiply(firstOperand, secondOperand);
+        break;
+    
+    case "/":
+        return divide(firstOperand, secondOperand);
+        break;
+   }
+};
